09c60684eb9a9450cb670f3f068c338980b8b3ad
EiffL <fr.eiffel@gmail.com>
2026-02-10 20:19:15 +0100

    Add Euclid VIS test data via Git LFS

---
+++ data/EUC_VIS_SWL/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff7ed63561e3d143b06409bd7d74f197428484a800c2d31da49aeb2644f17478
+size 6207
---
+++ data/EUC_VIS_SWL/prepare_quadrant_data.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:84cf1e7590c82c44182e493509be4e0be801382e2e6dcccfd16f1d859adb6cf1
+size 8936
