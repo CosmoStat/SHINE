3dbeaa08f7f7422922c910208962c433c4caedaf
EiffL <fr.eiffel@gmail.com>
2026-02-10 21:24:13 +0100

    updating data

--- data/EUC_VIS_SWL/prepare_quadrant_data.py
+++ data/EUC_VIS_SWL/prepare_quadrant_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84cf1e7590c82c44182e493509be4e0be801382e2e6dcccfd16f1d859adb6cf1
-size 8936
+oid sha256:008da7b3d5f4e3b31805944682ef3c71b6834cf12c729861cf302cd74de9e127
+size 11204
